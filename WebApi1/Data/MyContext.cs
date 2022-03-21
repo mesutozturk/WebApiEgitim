e2f66fb5a40632ae37d8682d405fa2920119b264
Mesut Öztürk <mesutozturk@users.noreply.github.com>
2022-03-21 13:41:50 +0300

    .

--- WebApi1/Data/MyContext.cs
+++ WebApi1/Data/MyContext.cs
@@ -7,9 +7,7 @@ public class MyContext : DbContext
 {
     public MyContext(DbContextOptions<MyContext> options)
     :base(options)
-    {
-        
-    }
+    { }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
